631a74e19a683104abbebedbfb17ec3686e0435a
LugariGabriele <161489816+LugariGabriele@users.noreply.github.com>
2024-03-14 17:43:36 +0100

    Update TenRun.java

--- array/TenRun.java
+++ array/TenRun.java
@@ -1,5 +1,8 @@
 package array;
-
+/*
+For each multiple of 10 in the given array, change all the values following it to be that multiple of 10, 
+until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+*/
 public class TenRun {
     public static int[] tenRun(int[] v){
     int [] dst= new int[v.length];
