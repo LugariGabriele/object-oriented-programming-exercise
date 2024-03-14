aacecdef1d5358761ff72b6fc1d293787f3b16af
LugariGabriele <161489816+LugariGabriele@users.noreply.github.com>
2024-03-14 17:42:09 +0100

    Update IsEveryWhere.java

--- array/IsEveryWhere.java
+++ array/IsEveryWhere.java
@@ -1,5 +1,14 @@
 package array;
+/*
+We'll say that a value is "everywhere" in an array if for every pair of adjacent elements in the array,
+at least one of the pair is that value. Return true if the given value is everywhere in the array.
 
+Examples:
+
+isEverywhere([1, 2, 1, 3], 1) → true
+isEverywhere([1, 2, 1, 3], 2) → false
+isEverywhere([1, 2, 1, 3, 4], 1) → false
+    */
 public class IsEveryWhere {
     public static boolean isEverywhere(int[] v, int value) {
         boolean ovunque = true;
