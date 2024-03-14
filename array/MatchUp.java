77a1587ad4b6b8f05213a258cc6d919748ed5d1e
LugariGabriele <161489816+LugariGabriele@users.noreply.github.com>
2024-03-14 17:44:34 +0100

    Update MatchUp.java

--- array/MatchUp.java
+++ array/MatchUp.java
@@ -1,5 +1,9 @@
 package array;
-
+/*
+Given two int[] v1 and v2 of the same length, for every element in v1, 
+consider the corresponding element in v2 (at the same index).
+Return the count of the number of times that the two elements differ by 2 or less, but are not equal.
+    */
 public class MatchUp {
     public static int matchUp(int[] v1, int[] v2){
     int Count=0;
