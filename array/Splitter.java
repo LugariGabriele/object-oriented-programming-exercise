8747030bd950e9314e22772514b7238990cccb66
LugariGabriele <161489816+LugariGabriele@users.noreply.github.com>
2024-03-14 17:40:52 +0100

    Update Splitter.java

--- array/Splitter.java
+++ array/Splitter.java
@@ -1,5 +1,11 @@
 package array;
+//Write a method accepting an int and returning the sequence of the digits as an int[].
 
+/*Examples:
+
+321 → [3, 2, 1]
+123 → [1, 2, 3]
+ */
 public class Splitter {
     public static int[] splitter(int input){
     String StringNumbers= Integer.toString(input);
