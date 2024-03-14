b7073567722da083600d1a43621d3255a36ccb08
LugariGabriele <161489816+LugariGabriele@users.noreply.github.com>
2024-03-14 17:29:54 +0100

    Update RemoveFirstTwoCharsIf.java

--- strings/RemoveFirstTwoCharsIf.java
+++ strings/RemoveFirstTwoCharsIf.java
@@ -1,5 +1,7 @@
 package strings;
-
+//Write a method accepting a string and returning the same string 
+//without the first two characters. Keep the first character if it is 'H' 
+//and keep the second character if it is 'e' (see StringBuilder class).
 public class RemoveFirstTwoCharsIf {
     public static String removeFirstTwoCharsIf(String string) {
         StringBuilder sb = new StringBuilder();
